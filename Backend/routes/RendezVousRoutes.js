ebf4b56ccb522625197d9c8056caf82066a7111c
fredericbob <lalafrederic05@gmail.com>
2025-03-16 09:11:26 +0300

    frederic

--- Backend/routes/RendezVousRoutes.js
+++ Backend/routes/RendezVousRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { addRendezVous, getRendezVous, deleteRendezVous, updateRendezVous } = require('../controllers/Rendezvous/RendezVousControllers');
 const jwtAuth = require('../middlewares/jwtAuth');
 
-router.post('/', addRendezVous);
+router.post('/',jwtAuth('manager'), addRendezVous);
 router.get('/', getRendezVous);
 router.delete('/:id', deleteRendezVous);
 router.put('/:id', updateRendezVous);
