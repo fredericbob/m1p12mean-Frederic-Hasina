e4a2faf02fd706612a2f2386df8f1d1057850555
Hasina <rhasina321@gmail.com>
2025-09-15 11:40:27 +0300

    maj route prestation manager

--- Backend/routes/manager/ServiceProposeRoutes.js
+++ Backend/routes/manager/ServiceProposeRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const ServiceProposeController = require('../../controllers/manager/ServiceProposeControllers');
-const jwtAuth=require('../../middlewares/jwtAuth');
+const jwtAuth = require('../../middlewares/jwtAuth');
 
+// Routes spécifiques AVANT les routes avec paramètres
+router.get('/pieces', jwtAuth('manager'), ServiceProposeController.getAllPieces);
+router.get('/types-vehicule', jwtAuth('manager'), ServiceProposeController.getAllTypesVehicule);
 
+// Routes CRUD générales
 router.post('/', jwtAuth('manager'), ServiceProposeController.createPrestation);
-router.get('/', jwtAuth('manager'),ServiceProposeController.getAllPrestations);
-router.get('/:id',jwtAuth('manager'), ServiceProposeController.getPrestationById);
-router.put('/:id',jwtAuth('manager'), ServiceProposeController.updatePrestation);
-router.delete('/:id',jwtAuth('manager'), ServiceProposeController.deletePrestation);
-router.get('/pieces',jwtAuth('manager'), ServiceProposeController.getAllPieces);
+router.get('/', jwtAuth('manager'), ServiceProposeController.getAllPrestations);
+router.get('/:id', jwtAuth('manager'), ServiceProposeController.getPrestationById);
+router.put('/:id', jwtAuth('manager'), ServiceProposeController.updatePrestation);
+router.delete('/:id', jwtAuth('manager'), ServiceProposeController.deletePrestation);
 
 module.exports = router;
